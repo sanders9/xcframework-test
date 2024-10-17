3376f39307f3a885be980a26c939629fa0e27e0c
Sanders MARTINE <sanders.martine9@gmail.com>
2024-10-17 11:39:19 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,8 +12,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "shared",
-//         url: "https://github.com/sanders9/xcframework-test/releases/download/test-0.0.1/shared.xcframework.zip",
-         url: "https://drive.google.com/file/d/169IaA5YFp0A4T_i0YZeqto1B4h7KK9YH/view?usp=drive_link",
+         url: "https://github.com/sanders9/xcframework-test/releases/download/test-0.0.1/shared.xcframework.zip"
          checksum:"4e093c1a30c25c9dfa1a757db1cf425d8540f6127d5f31f82c66d662dab32b65"
          )
    ]
